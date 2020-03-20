6d6222f794e3cb2827b4a0f6e434d29c8ef0ea48
musfiqurcse <md.musfiqurcse@outlook.com>
2020-03-20 22:11:35 +0100

    Try catch added for error detection

--- solution.py
+++ solution.py
@@ -4,16 +4,6 @@ import matplotlib.pyplot as plt
 print('Give the path of Image: \n')
 path_of_file = input()
 image_input = cv2.imread(path_of_file)
-# try:
-#     # Image path: /home/musfiq/Public/codebase/assets/test.jpeg
-#     print
-#     cv2.imshow("Preview", image_input)
-# except Exception as ex:
-#     print(ex)
-#     print('File Not Found')
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-# cv2.waitKey(1)
 while True:
     """
     Instruction:
@@ -24,15 +14,20 @@ while True:
     Instruction:
     Show Only Greyscale **ENTER 1**
     Dynamically Change Thresohlding **ENTER 2**
+    Exit the Instruction Process **ENTER 0**
     """)
     input_inst = int(input())
-    if input_inst == 1:
-        grey_scale_image = cv2.cvtColor(image_input, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('Main Image', image_input)
-        cv2.imshow('Grey Scale Image', grey_scale_image)
-        cv2.waitKey(0)
-    elif input_inst == 0:
-        cv2.destroyAllWindows()
-        cv2.waitKey(1)
-        break
-print('it is working')
+    try:
+        if input_inst == 1:
+            grey_scale_image = cv2.cvtColor(image_input, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('Main Image', image_input)
+            cv2.imshow('Grey Scale Image', grey_scale_image)
+            cv2.waitKey(0)
+        elif input_inst == 0:
+            cv2.destroyAllWindows()
+            cv2.waitKey(1)
+            break
+    except Exception as ex:
+        print('Error Occurred')
+        print(ex)
+print('Application is closing')
